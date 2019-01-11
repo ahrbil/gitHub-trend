0e95d83ac5a1c8f567f5c9e525742ab60440c8e0
ahrbil <ahrbil.me@gmail.com>
2019-01-11 18:44:12 +0100

    setting up eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-const App = () => (
-  <h1>hello</h1>
-)
+const App = () => <h1>hello</h1>;
 
 export default App;
